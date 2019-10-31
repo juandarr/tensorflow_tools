d9684411392ffb87b08fbc154043f22c4e7fa5a2
Juan David Rios <juandarr@gmail.com>
2019-10-31 14:14:53 -0500

    fix: variables redefined to fit the requirements of the new tf records creation.

--- json_to_csv.py
+++ json_to_csv.py
@@ -9,14 +9,14 @@ Path is defined as an argument here
 '''
 def json_to_csv(path):
     json_list = []
-    for json_file in glob.glob(path + '/*.json'):
+    for json_file in glob.glob(path + '*.json'):
         with open(json_file, 'r') as f:
             datastore = json.load(f)
             print('Here is the Json file: ', json_file)
             filename = json_file.split('/')[-1].split('.')[0]
             print('And this is the filename: ', filename)
             filename += '.jpg'
-            im = Image.open(filename)
+            im = Image.open(path+filename)
             width, height = im.size
             for data in datastore:
                 value = (filename,
@@ -34,7 +34,8 @@ def json_to_csv(path):
     return json_df
 
 def main():
-    image_path = os.path.join(os.getcwd(), 'annotations')
+    image_path = os.path.join(os.getcwd(), 'train/')
+    print('This is the image path: ', image_path)
     json_df = json_to_csv(image_path)
     json_df.to_csv('train_labels.csv', index=None)
     print('Successfully converted json to csv.')
