a3768e79f1fc363419445f72c5a7105a39668114
Juan David Rios <juandarr@gmail.com>
2019-10-31 12:20:02 -0500

    refactor: variables renamed for clarity and consistency with the script process.

--- json_to_csv.py
+++ json_to_csv.py
@@ -30,13 +30,13 @@ def json_to_csv(path):
                         )
                 json_list.append(value)
     column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
-    xml_df = pd.DataFrame(json_list, columns=column_name)
-    return xml_df
+    json_df = pd.DataFrame(json_list, columns=column_name)
+    return json_df
 
 def main():
     image_path = os.path.join(os.getcwd(), 'annotations')
-    xml_df = json_to_csv(image_path)
-    xml_df.to_csv('raccoon_labels.csv', index=None)
+    json_df = json_to_csv(image_path)
+    json_df.to_csv('train_labels.csv', index=None)
     print('Successfully converted json to csv.')
 
 main()
